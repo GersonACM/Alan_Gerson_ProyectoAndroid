4d02ef382e0c24211c2b371625283230026e9851
GersonACM <98502358+GersonACM@users.noreply.github.com>
2022-05-27 20:47:18 -0500

    proyecto1

--- app/src/main/java/Proyecto/Activity/MainActivity.kt
+++ app/src/main/java/Proyecto/Activity/MainActivity.kt
@@ -2,11 +2,19 @@ package Proyecto.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottonNavigationView)
+        val navController = findNavController(R.id.fragment)
+        bottomNavigationView.setupWithNavController(navController)
+        
 
     }
 }
