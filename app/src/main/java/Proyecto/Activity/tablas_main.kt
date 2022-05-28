676550d8a1f4080cb78f5828f9b06e79b9a7c2d6
GersonACM <98502358+GersonACM@users.noreply.github.com>
2022-05-27 21:10:01 -0500

    proyecto2

--- app/src/main/java/Proyecto/Activity/tablas_main.kt
+++ app/src/main/java/Proyecto/Activity/tablas_main.kt
@@ -1,59 +1,33 @@
 package Proyecto.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [tablas_main.newInstance] factory method to
- * create an instance of this fragment.
- */
 class tablas_main : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    lateinit var btnPracticar: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tablas_main, container, false)
+
+        val vista = inflater.inflate(R.layout.fragment_tablas_main, container, false)
+        btnPracticar = vista.findViewById(R.id.btnPracticar)
+
+        btnPracticar.setOnClickListener {
+            findNavController().navigate(R.id.action_tablas_main2_to_tablas_pract)
+        }
+        return vista
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment tablas_main.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            tablas_main().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- app/src/main/java/Proyecto/Activity/tablas_pract.kt
+++ app/src/main/java/Proyecto/Activity/tablas_pract.kt
@@ -1,59 +1,73 @@
 package Proyecto.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [tablas_pract.newInstance] factory method to
- * create an instance of this fragment.
- */
 class tablas_pract : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var tvFactor1: TextView
+    lateinit var tvFactor2: TextView
+    lateinit var Respuesta: EditText
+    lateinit var btnRespuesta: Button
+    var factor1:Int = 0
+    var factor2:Int = 0
+    var producto:Int = 0
+    var respuestaCorrecta = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tablas_pract, container, false)
+        val vista = inflater.inflate(R.layout.fragment_tablas_pract, container, false)
+        initUI()
+
+        btnRespuesta.setOnClickListener {
+            var strRespuesta = Respuesta.text.toString()
+            if(strRespuesta.equals("")){
+                Toast.makeText(this, "Respuesta Vacia", Toast.LENGTH_SHORT).show()
+
+            }else{
+                respuestaCorrecta = producto == strRespuesta.toInt()
+                generaMultiplicacion()
+                val mostrarResultado = Intent(this, ResultadoActivity::class.java)
+                mostrarResultado.putExtra("respuesta",respuestaCorrecta)
+                startActivity(mostrarResultado)
+
+            }
+
+        }
+
+    }
+    fun initUI(){
+        tvFactor1 = findViewById(R.id.tvFactor1)
+        tvFactor2 = findViewById(R.id.tvFactor2)
+        Respuesta = findViewById(R.id.Respuest)
+        btnRespuesta = findViewById(R.id.btnResponder)
+
+        generaMultiplicacion()
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment tablas_pract.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            tablas_pract().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun generaMultiplicacion(){
+        factor1 = Random.nextInt(0,10)
+        factor2 = Random.nextInt(0,10)
+        producto = factor1*factor2
+        tvFactor1.text = "$factor1"
+        tvFactor2.text = "$factor2"
+        Respuesta.text.clear()
+
     }
+        return vista
+    }
+
+
 }
